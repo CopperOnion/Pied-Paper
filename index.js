b1b8a525832a2ce9191cf689477ea8735a255e49
dhyunpark <danpark0605@gmail.com>
2020-07-31 00:57:21 +0900

    added database files following the tutorial, getting ECONNREFUSED ERROR

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const db = require("./queries");
 
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
-
+// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 // An api endpoint that returns a short list of items
 app.get("/api/getList", (req, res) => {
   var list = ["item1", "item2", "item3"];
@@ -14,9 +21,11 @@ app.get("/api/getList", (req, res) => {
 });
 
 // Handles any requests that don't match the ones above
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
---
+++ queries.js
@@ -0,0 +1,81 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "me",
+  host: "localhost",
+  database: "api",
+  password: "password",
+  port: 5432,
+});
+
+const getUsers = (req, res) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, result) => {
+    if (error) {
+      throw error;
+    }
+
+    res.status(200).json(result.rows);
+  });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, result) => {
+    if (error) {
+      throw error;
+    }
+
+    res.status(200).json(result.rows);
+  });
+};
+
+const createUser = (req, res) => {
+  const { name, email } = req.body;
+
+  pool.query(
+    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+    [name, email],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`User added with ID: ${result.insertId}`);
+    }
+  );
+};
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, email } = req.body;
+
+  pool.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+    [name, email, id],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, result) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
