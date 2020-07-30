3028c33c84d4dde73aec062c9965c34c319e43f7
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-07-30 16:45:56 -0400

    Testing GET http method from postgres heroku

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const db = require("./queries");
+const cors = require("cors");
+
+const news = require("./routes/news");
 
 const app = express();
 
 // Serve the static files from the React app
-// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -20,14 +24,31 @@ app.get("/api/getList", (req, res) => {
   console.log("Sent list of items");
 });
 
-// Handles any requests that don't match the ones above
-app.get("/users", db.getUsers);
-app.get("/users/:id", db.getUserById);
-app.post("/users", db.createUser);
-app.put("/users/:id", db.updateUser);
-app.delete("/users/:id", db.deleteUser);
 
+// Cors
+app.use(cors());
+app.options('*', cors());
+
+// Routes
 const port = process.env.PORT || 5000;
+
+if ( process.env.PORT){
+  // Handles any requests that don't match the ones above
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+  app.use("/api/news", news);  
+}
+else{
+  app.get("/api/news/users", db.getUsers);
+  /* app.get("/users", localdb.getUsers);
+  app.get("/users/:id", localdb.getUserById);
+  app.post("/users", localdb.createUser);
+  app.put("/users/:id", localdb.updateUser);
+  app.delete("/users/:id", localdb.deleteUser); */
+
+}
+
 app.listen(port);
 
 console.log("App is listening on port " + port);
---
+++ routes/news.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "wejgjhiuiahuhh",
+    host: "ec2-34-193-117-204.compute-1.amazonaws.com",
+    database: "de20i26pohuek0",
+    password: "0412",
+    port: 5432,
+    sslmode:require
+  });
+
+router.get("/users" ,(req, res) => {
+    pool.query("SELECT * FROM users ORDER BY id ASC", (error, result) => {
+        if (error) {
+            throw error;
+        }
+
+        res.status(200).json(result.rows);
+    });
+})
+
+
+module.exports = router;
