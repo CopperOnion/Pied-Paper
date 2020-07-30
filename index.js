03021f41aa354a9bda3161165805d775ea7e4642
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-07-30 15:51:08 -0400

    Express needs to be statically serving files

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-/* const express = require("express");
-const path = require("path");
-
-const app = express();
-
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
-
-// An api endpoint that returns a short list of items
-app.get("/api/getList", (req, res) => {
-  var list = ["item1", "item2", "item3"];
-  res.json(list);
-  console.log("Sent list of items");
-});
-
-// Handles any requests that don't match the ones above
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
-
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log("App is listening on port " + port);
- */
-
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -32,7 +6,8 @@ const db = require("./queries");
 const app = express();
 
 // Serve the static files from the React app
-// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -52,6 +27,11 @@ app.get("/users/:id", db.getUserById);
 app.post("/users", db.createUser);
 app.put("/users/:id", db.updateUser);
 app.delete("/users/:id", db.deleteUser);
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
