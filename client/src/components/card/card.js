1f6cf9f79db3a6c46bc675d1a2311cdcdb72862f
Donghyun Park <danpark0605@gmail.com>
2020-08-20 23:26:49 +0900

    refactored card's voting functionality into the card.js, and restructured the files for easier workflow in the future

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import './card.css'
+
+import axios from 'axios';
 
 
 const useStyles = makeStyles({
@@ -13,19 +15,19 @@ const useStyles = makeStyles({
       marginBottom: "1em"
     },
     display: 'flex',
-    flexDirection:'row',
+    flexDirection: 'row',
     padding: 0,
-    transition:'transform .4s',
-    overflow:'visible',
-    height:"20vh",
-    marginBottom:"2vh"
+    transition: 'transform .4s',
+    overflow: 'visible',
+    height: "20vh",
+    marginBottom: "2vh"
   },
   '.MuiCardContent-root:last-child': {
     padding: 0
   },
 
   'root:hover': {
-    
+
   },
   media: {
     height: '20vh',
@@ -46,49 +48,100 @@ const useStyles = makeStyles({
   },
 
 });
+
 /* 
 TODO: Add a detail tab that expands upon hover ( separate from the original card)
 
 */
-export default function MediaCard({ title, description, image, theme ,date}) {
+export default function MediaCard({ title, description, url, i, image, theme, date }) {
   const classes = useStyles();
+  const [current_true, setTrue] = useState(0)
+  const [current_false, setFalse] = useState(0)
+
+  const showmore = useCallback((index) => {
+    var element = document.getElementsByClassName(index);
+    element[0].classList.remove("notdisplayed");
+    element[0].classList.add("displayed");
+  }, []);
+
+  const showless = useCallback((index) => {
+    var element = document.getElementsByClassName(index);
+    element[0].classList.remove("displayed");
+    element[0].classList.add("notdisplayed");
+  }, []);
+
+  //handler for user voting on articles
+  const uservote = useCallback((votetype, url, i) => {
+    axios
+      .post('/api/news/uservote', {
+        headers: { Accept: 'application/json' },
+        params: {
+          url: url,
+          type: votetype
+        }
+      })
+      .then(res => {
+        /*
+            res = {
+                user_true: 123,
+                user_false: 123
+            }
+        */
+        console.log(res.data.votes)
+        setTrue(res.data.votes.user_true)
+        setFalse(res.data.votes.user_false)
+        showmore("stats" + i)
+      })
+  }, []);
 
   return (
-    <ThemeProvider theme={theme}>
+    <div>
+      <a onClick={() => showmore('attached' + i)} href={url} target="_blank">
+        <ThemeProvider theme={theme}>
+          <Card elevation={0} className={classes.root}>
 
-      <Card elevation={0} className={classes.root}>
+            <div className={classes.content}>
+              <div className={classes.title}>
+                <h3>{title}</h3>
+                <h6>{date}</h6>
+
+              </div>
+
+              <p style={{ marginBottom: "50px" }}>
+                {description}
+              </p>
 
 
-          <div className={classes.content}>
-            <div className={classes.title}>
-              <h3>  {title} </h3>
-              <h6>{date}</h6>
-
+              <h4>Lot's of disagreement</h4>
             </div>
 
+            <CardMedia
+              className={classes.media}
+              image={image}
+              title="Contemplative Reptile"
+            />
 
-            <p style={{marginBottom:"50px"}}>
-              {description}
-            </p>
+          </Card>
+        </ThemeProvider>
+      </a>
 
-            
-            <h4>Lot's of disagreement</h4>
+      <div className={'attached' + i + ' notdisplayed'}>
+        <div className='details'>
+          <h3>Do you think this article is <button className='details_buttons' onClick={() => uservote("user_true", url, i)}>True News</button> or <button className='details_buttons' onClick={() => uservote("user_false", url, i)}>Fake News</button> ?</h3>
+
+          <div className={'stats' + i + ' notdisplayed'}>
+            {current_true}
+            {current_false}
           </div>
 
+          {/* Do not know if we need these anymore
+            <h4 onMouseOver={() => this.showmore("source" + i)} >Hover to see source</h4>
+            <h4 className={`source` + i + ' notdisplayed'}>{e.articles.source.name}</h4>  
+          */}
+        </div>
+        <button onClick={() => showless("attached" + i)}>Hide</button>
 
-
-
-          <CardMedia
-            className={classes.media}
-            image={image}
-            title="Contemplative Reptile"
-          />
-
-        
-
-
-      </Card>
-    </ThemeProvider>
-
+      </div>
+    </div>
   );
 }
--- client/src/components/content/team/team.js
+++ client/src/components/content/team/team.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import { ThemeProvider } from '@material-ui/styles';
 
-import './about.css'
-function Team( {theme}) {
+import './team.css'
+function Team({ theme }) {
     return (
         <ThemeProvider theme={theme}>
             <div className="about">
-                <Typography  style={{margin: '4vw'}}variant="h1" color='secondary'>
+                <Typography style={{ margin: '4vw' }} variant="h1" color='secondary'>
                     Team members
+                    <ul>
+                        <li>Donghyun Park</li>
+                        <li>Minyoung Na</li>
+                        <li>Paul Kang</li>
+                    </ul>
                 </Typography>
             </div>
         </ThemeProvider>
